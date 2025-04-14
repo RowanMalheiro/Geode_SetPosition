585b009ead285c2e1416eb866d12314faa4d747b
rowanmalheiro <rowan.malheiro@live.ca>
2025-04-13 23:21:17 -0400

    Reformatted and added support for float-valued positions

---
+++ src/includes.hpp
@@ -0,0 +1,28 @@
+#include <Geode/Geode.hpp>
+#include <Geode/cocos/include/ccTypes.h>
+#include <Geode/modify/PlayerObject.hpp>
+#include <Geode/modify/GJBaseGameLayer.hpp>
+#include <Geode/modify/PlayLayer.hpp>
+#include <Geode/modify/PauseLayer.hpp>
+#include <Geode/ui/Layout.hpp>
+#include <Geode/cocos/robtop/xml/pugixml.hpp>
+#include <String>
+
+#define FONT "bigFont.fnt"
+#define VALIDCHARS "0123456789."
+
+using namespace geode::prelude;
+
+struct Vec3{
+    float x;
+    float y;
+    float z;
+};
+
+class ModData{
+    public:
+    static inline Vec3 pos = {0.f,0.f,0.f};
+    static inline bool isActive = false;
+    static inline void* curPlay = nullptr;
+
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,16 @@
-#include <Geode/Geode.hpp>
-#include <Geode/modify/PlayerObject.hpp>
-#include <Geode/modify/GJBaseGameLayer.hpp>
-#include <Geode/modify/PlayLayer.hpp>
-#include <Geode/modify/PauseLayer.hpp>
-#include <Geode/ui/Layout.hpp>
-#include <String>
+#include "includes.hpp"
 
-#define FONT "bigFont.fnt"
 
-using namespace geode::prelude;
-
-CCPoint pos = {100, 2000};
-double rot = 0;
-bool active = false;
-PlayerObject* cur_play = nullptr;
+$execute{
+   
+   ModData::pos=Vec3{
+      .x = 0,
+      .y = 105,
+      .z = 0
+   };
+   ModData::isActive= false;
+   ModData::curPlay = nullptr;
+};
 
 // specify parameters for the setup function in the Popup<...> template
 class settingsPopup : public geode::Popup<std::string const&> {
@@ -22,8 +19,11 @@ class settingsPopup : public geode::Popup<std::string const&> {
 
        bool setup(std::string const&) override {
          this->setZOrder(100);
-         m_x=pos.x;m_y=pos.y; m_rot=rot;
 
+         m_x=ModData::pos.x;
+         m_y=ModData::pos.y;
+         m_rot=ModData::pos.z;
+         
          m_mainLayer->setTouchEnabled(true);
          // convenience function provided by Popup
          // for adding/setting a title to the popup
@@ -42,24 +42,30 @@ class settingsPopup : public geode::Popup<std::string const&> {
          menu->setContentSize(m_mainLayer->getContentSize());
          popupmenu->addChildAtPosition(menu, Anchor::Center);
 
-         auto xbox = TextInput::create(80, std::to_string(m_x), FONT);
-         auto ybox = TextInput::create(80, std::to_string(m_y), FONT);
-         auto rbox = TextInput::create(80, std::to_string(m_rot), FONT);
+         auto xbox = TextInput::create(80, std::to_string(m_x).substr(0,8), FONT);
+         auto ybox = TextInput::create(80, std::to_string(m_y).substr(0,8), FONT);
+         auto rbox = TextInput::create(80, std::to_string(m_rot).substr(0,8), FONT);
+         xbox->setFilter(VALIDCHARS);
+         ybox->setFilter(VALIDCHARS);
+         rbox->setFilter(VALIDCHARS);
+         
          xbox->setCallback([&](const std::string& txt){
-            pos.x=(int)std::strtol(txt.c_str(), nullptr, 10);
+            m_x=(float)std::strtod(txt.c_str(), nullptr);
+            ModData::pos.x = m_x;
          });
          ybox->setCallback([&](const std::string& txt){
-            pos.y=(int)std::strtol(txt.c_str(), nullptr, 10);
+            m_y=(float)std::strtod(txt.c_str(), nullptr);
+            ModData::pos.y=m_y;
          });
          rbox->setCallback([&](const std::string& txt){
-            rot=(float)std::strtol(txt.c_str(), nullptr, 10);
-
+            m_rot=(float)std::strtod(txt.c_str(), nullptr);
+            ModData::pos.z=m_rot;
          });
          menu->addChild(xbox);
          menu->addChild(ybox);
          menu->addChild(rbox);
 
-         auto checkBox = CCSprite::createWithSpriteFrameName(active ? "GJ_checkOn_001.png" : "GJ_checkOff_001.png");
+         auto checkBox = CCSprite::createWithSpriteFrameName(ModData::isActive ? "GJ_checkOn_001.png" : "GJ_checkOff_001.png");
          auto set = CCMenuItemSpriteExtra::create(checkBox, nullptr, m_mainLayer, menu_selector(settingsPopup::onSet));
          set->setPosition({menu->getPositionX(),menu->getPositionY()-40.f});
          set->setID("sp-setbtn");
@@ -68,11 +74,18 @@ class settingsPopup : public geode::Popup<std::string const&> {
          return true;
        }
 
+       void updateSavedPos(){
+         ModData::pos.x=m_x;
+         ModData::pos.y=m_y;
+         ModData::pos.z=m_rot;
+       }
+       
+
        
    
    public:
 
-      int m_x, m_y, m_rot;
+      float m_x, m_y, m_rot;
 
 
        static settingsPopup* create(std::string const& text) {
@@ -88,8 +101,8 @@ class settingsPopup : public geode::Popup<std::string const&> {
       }
        void onSet(CCObject* obj){
          auto cckbx = static_cast<CCMenuItemSpriteExtra*>(obj);
-         active = !active;
-         cckbx->setSprite(CCSprite::createWithSpriteFrameName(active ? "GJ_checkOn_001.png" : "GJ_checkOff_001.png"));
+         ModData::isActive = !ModData::isActive;
+         cckbx->setSprite(CCSprite::createWithSpriteFrameName(ModData::isActive ? "GJ_checkOn_001.png" : "GJ_checkOff_001.png"));
        }
 };
 
@@ -97,12 +110,18 @@ class $modify(playobj, PlayerObject) {
    
    //put the icon at pos
    void update(float t){
-      if(!active){
+      if(!ModData::isActive){
          return update(t);
       }
-      setPosition(pos);
-      setRotation(rot);
+      setPos();
       PlayerObject::update(0.0f);
+   }
+
+   void setPos(){
+      auto pos = ModData::pos;
+      CCPoint posxy {pos.x,pos.y};
+      setPosition(posxy);
+      setRotation(pos.z);
    }
 };
 
@@ -114,20 +133,22 @@ class $modify(PlayLayer){
 
    bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects){
       bool b = PlayLayer::init(level, useReplay, dontCreateObjects);
-      cur_play = nullptr;
+      ModData::curPlay = nullptr;
       m_fields->m_play = nullptr;
-      active = false;
+      ModData::isActive = false;
       return b;
    }
 
    void destroyPlayer(PlayerObject* p, GameObject* g){ 
+      Vec3 tempPos = ModData::pos;
 
-      if(cur_play == nullptr){
-         cur_play = p;
+      if(ModData::curPlay == nullptr){
+         ModData::curPlay = p;
       }
       if(m_fields->m_play == nullptr){
          m_fields->m_play=static_cast<PlayerObject*>(g);
-         p->getPosition(&pos.x, &pos.y);
+         p->getPosition(&tempPos.x, &tempPos.y);
+         ModData::pos = tempPos;
          destroyPlayer(p,g);
          return;
       }
@@ -135,15 +156,17 @@ class $modify(PlayLayer){
    }
 
    void resetLevel(){
+      Vec3 tpos = ModData::pos;
       PlayLayer::resetLevel();
-      if(cur_play != nullptr){
-         cur_play->getPosition(&pos.x, &pos.y);
+      if(ModData::curPlay != nullptr){
+         ((PlayerObject*)ModData::curPlay)->getPosition(&tpos.x, &tpos.y);
+         ModData::pos=tpos;
       }
       return;
    }
 
    void onExit(){
-      cur_play=nullptr;
+      ModData::curPlay = nullptr;
       PlayLayer::onExit();
    }
 };
@@ -151,7 +174,7 @@ class $modify(PlayLayer){
 class $modify(gamelay, GJBaseGameLayer){
 
    void update(float f){
-      if(!active){
+      if(!ModData::isActive){
          return GJBaseGameLayer::update(f);
       }
       updateCamera(0.001f);
