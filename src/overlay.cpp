70a7ba85ab01090376baf854f2f505642fddea54
rowanmalheiro <rowan.malheiro@live.ca>
2025-11-17 15:52:35 -0500

    add slider controls

--- src/overlay.cpp
+++ src/overlay.cpp
@@ -1,5 +1,7 @@
 #include "overlay.hpp"
 
+#define SLIDER_RANGE 0.5f
+
 static Overlay* s_overlay = nullptr;
 
 Overlay* Overlay::get(){
@@ -27,6 +29,11 @@ void Overlay::draw(){
     static char xbuf[32] = "";
     static char ybuf[32] = "";
     static char rbuf[32] = "";
+    static float sliderspeed = 1.0f;
+
+    float tempx = ModData::pos.x;
+    float tempy = ModData::pos.y;
+    float tempz = ModData::pos.z;
 
     strcpy(xbuf, std::to_string(ModData::pos.x).c_str());
     strcpy(ybuf, std::to_string(ModData::pos.y).c_str());
@@ -39,12 +46,31 @@ void Overlay::draw(){
 
     ImGui::Begin("SetPosition");
     ImGui::InputText("x pos", xbuf, IM_ARRAYSIZE(xbuf), ImGuiInputTextFlags_CharsDecimal);
+    ImGui::SliderFloat("x slider", &tempx, ModData::pos.x-sliderspeed, ModData::pos.x+sliderspeed);
     ImGui::InputText("y pos", ybuf, IM_ARRAYSIZE(ybuf), ImGuiInputTextFlags_CharsDecimal);
+    ImGui::SliderFloat("y slider", &tempy, ModData::pos.y-sliderspeed, ModData::pos.y+sliderspeed);
     ImGui::InputText("rot", rbuf, IM_ARRAYSIZE(rbuf), ImGuiInputTextFlags_CharsDecimal);
+    ImGui::SliderFloat("rot slider", &tempz, ModData::pos.z-sliderspeed, ModData::pos.z+sliderspeed);
+    ImGui::SliderFloat("Slider Speed", &sliderspeed, 0.0f, 10.f);
 
     ImGui::Selectable("Enable Mod", &ModData::isActive);
 
     ImGui::End();
+
+    //wtf is this code
+    //worst code ever?
+    if(tempx != ModData::pos.x){
+        ModData::pos.x = tempx;
+        return;
+    }
+    if(tempy != ModData::pos.y){
+        ModData::pos.y = tempy;
+        return;
+    }
+    if(tempz != ModData::pos.z){
+        ModData::pos.z = tempz;
+        return;
+    }
 
     ModData::pos.x = std::stof(xbuf);
     ModData::pos.y = std::stof(ybuf);
